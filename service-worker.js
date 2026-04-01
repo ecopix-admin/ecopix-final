a91c428ec3b48d6c216c01aa28326ae6f301e15b
ecopix-admin <hesenovhasan735@gmail.com>
2026-04-01 23:27:23 +0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'az-v1';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/app.js', '/p2p-engine.js'])));
-});
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
